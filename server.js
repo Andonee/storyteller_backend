88b0a87f434d6c121f63fc2bdeedbfaee4dda755
mpoprawa <mateusz.poprawa@gmail.com>
2021-12-20 21:41:00 +0100

    connect to DB

---
+++ server.js
@@ -0,0 +1,20 @@
+const app = require('./src/app')
+const pool = require('./src/pool')
+require('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+
+pool
+	.connect({
+		host: 'localhost',
+		port: 5432,
+		database: 'storyteller',
+		user: process.env.pg_user,
+		password: process.env.pg_password,
+	})
+	.then(() => {
+		app().listen(PORT, () => {
+			console.log(`Listening on port ${PORT}`)
+		})
+	})
+	.catch(err => console.log(err))
